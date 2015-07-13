8130460195fa9feff17043adaf9556d5ea2a10e0
Ricardo Oyarzun <royarzun@gmail.com>
2015-07-13 11:29:23 -0300

    setup: Modify setup for clara setup.py compatibility

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 '''
-Created on 27-02-2015
+ Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
+ Permission to use, copy, modify, and distribute this software and its
+ documentation for educational, research, and not-for-profit purposes,
+ without fee and without a signed licensing agreement.
 
-@author: royarzun
+ Author Vardan Gyurjyan
+ Department of Experimental Nuclear Physics, Jefferson Lab.
+
+ IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+ INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+ THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
+ OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+ SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 '''
 from distutils.core import setup
 from distutils.command.clean import clean
@@ -22,6 +37,7 @@ class PyTest(TestCommand):
         import pytest
         pytest.main(self.test_args)
 
+
 class xMsgInstall(install):
 
     def run(self):
@@ -31,17 +47,19 @@ class xMsgInstall(install):
         c.finalize_options()
         c.run()
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     setup(name='xMsg',
-          version='1.0',
+          version='2.0',
           description='xMsg for python',
           author='Vardan Gyurgyan',
           author_email='vardan@jlab.org',
           url='https://claraweb.jlab.org',
-          test_suite = "test",
-          tests_require=['pytest'],
-          cmdclass = {'test': PyTest},
-          packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-          install_requires = ['pyzmq>=14.5.0','protobuf>=2.6','mock>=1.0.1'],
+          test_suite="tests",
+          tests_require=['pytest', 'mockito'],
+          cmdclass={'test': PyTest},
+          packages=find_packages(exclude=["*.tests", "*.tests.*",
+                                          "tests.*", "tests"]),
+          install_requires=['pyzmq>=14.5.0', 'protobuf>=2.6', 'mock>=1.0.1',
+                            'enum34>=1.0.4', 'argparse>=1.2.1', 'netifaces>=0.10.4']
           )
-
