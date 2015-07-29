d3d28bbc52de738954772c4be5aec040616f03bf
Ricardo Oyarzun <royarzun@gmail.com>
2015-07-28 22:04:27 -0300

    net: Update the docstrings for sphinx

--- xmsg/net/xMsgAddress.py
+++ xmsg/net/xMsgAddress.py
@@ -1,23 +1,24 @@
-'''
- Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
- Permission to use, copy, modify, and distribute this software and its
- documentation for educational, research, and not-for-profit purposes,
- without fee and without a signed licensing agreement.
+#
+# Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for educational, research, and not-for-profit purposes,
+# without fee and without a signed licensing agreement.
+#
+# Author Vardan Gyurjyan
+# Department of Experimental Nuclear Physics, Jefferson Lab.
+#
+# IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+# HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+#
 
- Author Vardan Gyurjyan
- Department of Experimental Nuclear Physics, Jefferson Lab.
-
- IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
- INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
- THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
- OF THE POSSIBILITY OF SUCH DAMAGE.
-
- JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
- HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
- SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-'''
 from xmsg.core.xMsgConstants import xMsgConstants
 from xmsg.core.xMsgUtil import xMsgUtil
 
@@ -25,39 +26,61 @@ __author__ = 'gurjyan'
 
 
 class xMsgAddress:
+    """xMsg network address container class.
 
+    Defines a key constructed as host:port (xMsg convention) for storing
+    xMsgConnection objects.
+
+    Attributes:
+        host (String): address host
+        port (int): address port
+        key (String): address dotted notation
     """
-      xMsg network address container class.
-      Defines a key constructed as host:port (xMsg
-      convention) for storing xMsgConnection objects.
-    """
 
-    host = str(xMsgConstants.UNDEFINED)
-    port = int(xMsgConstants.DEFAULT_PORT)
-    key = str(xMsgConstants.UNDEFINED)
+    def __init__(self, hostname="localhost",
+                 port=int(xMsgConstants.DEFAULT_PORT)):
+        """Constructor that converts host name into a dotted notation
+        of the IP address.
 
-    def __init__(self, host="localhost", port=int(xMsgConstants.DEFAULT_PORT)):
+        It creates an instance of the cMsgAddress using user provided host and
+        port. If port is not defined uses default port
+
+        Args:
+            hostname (String): hostname
+            port (int): port number
         """
-          Constructor that converts host name into a
-          dotted notation of the IP address.
-          It creates an instance of the cMsgAddress
-          using user provided host and port. If port
-          is not defined uses default port
-        :param host name
-        :param port port number
-        """
-        self.host = xMsgUtil.host_to_ip(host)
+        self.host = xMsgUtil.host_to_ip(hostname)
         self.port = port
-        self.key = self.host + ":" + str(self.port)
+        self.key = "%s:%s" % (self.host, str(self.port))
 
     def get_host(self):
+        """Returns the host ip address
+
+        Returns:
+            host (String): host ip address
+        """
         return self.host
 
     def get_port(self):
+        """Returns the address port
+
+        Returns:
+            port (int): host port
+        """
         return self.port
 
     def set_port(self, port):
+        """Sets the address port
+
+        Args:
+            port (int): address port
+        """
         self.port = port
 
     def get_key(self):
+        """Returns address generated key
+
+        Returns:
+            key (String): address generated key
+        """
         return self.key
--- xmsg/net/xMsgConnection.py
+++ xmsg/net/xMsgConnection.py
@@ -1,56 +1,89 @@
-'''
- Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
- Permission to use, copy, modify, and distribute this software and its
- documentation for educational, research, and not-for-profit purposes,
- without fee and without a signed licensing agreement.
+#
+# Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for educational, research, and not-for-profit purposes,
+# without fee and without a signed licensing agreement.
+#
+# Author Vardan Gyurjyan
+# Department of Experimental Nuclear Physics, Jefferson Lab.
+#
+# IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+# HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+#
 
- Author Vardan Gyurjyan
- Department of Experimental Nuclear Physics, Jefferson Lab.
-
- IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
- INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
- THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
- OF THE POSSIBILITY OF SUCH DAMAGE.
-
- JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
- HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
- SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-'''
 from xmsg.core.xMsgConstants import xMsgConstants
 
 __author__ = 'gurjyan'
 
 
 class xMsgConnection:
+    """xMsg connection class
+
+    Contains xMSgAddress object and two zmq socket objects for
+    publishing and subscribing xMsg messages respectfully.
+
+    Attributes:
+        address (xMsgAddress): xMsg address object
+        pub_sock (zmq.Socket): connection PUB socket
+        sub_sock (zmq.Socket): connection SUB socket
     """
-    xMsg connection class. Contains xMSgAddress object and
-    two zmq socket objects for publishing and subscribing
-    xMsg messages respectfully.
-    """
 
-    address = str(xMsgConstants.UNDEFINED)
-    pubSock = str(xMsgConstants.UNDEFINED)
-    subSock = str(xMsgConstants.UNDEFINED)
+    def __init__(self, address=str(xMsgConstants.UNDEFINED)):
+        self.pub_sock = str(xMsgConstants.UNDEFINED)
+        self.sub_sock = str(xMsgConstants.UNDEFINED)
 
-    def __init__(self):
-        pass
+    def set_address(self, address):
+        """Sets the connection address
 
-    def set_address(self, ad):
-        self.address = ad
+        Args:
+            address (xMsgAddress): xMsg address object
+        """
+        self.address = address
 
     def get_address(self):
+        """Returns connection address
+
+        Returns:
+            address (xMsgAddress): connection address
+        """
         return self.address
 
-    def set_pub_sock(self, soc):
-        self.pubSock = soc
+    def set_pub_sock(self, socket):
+        """Returns xMsg PUB socket
+
+        Args:
+            socket (zmq.Socket): connection PUB socket
+        """
+        self.pub_sock = socket
 
     def get_pub_sock(self):
-        return self.pubSock
+        """Returns xMsg PUB socket
 
-    def set_sub_sock(self, soc):
-        self.subSock = soc
+        Returns:
+            pub_sock (zmq.Socket): connection PUB socket
+        """
+        return self.pub_sock
+
+    def set_sub_sock(self, socket):
+        """Returns xMSg SUB socket
+
+        Args:
+            socket (zmq.Socket): connection SUB socket
+        """
+        self.sub_sock = socket
 
     def get_sub_sock(self):
-        return self.subSock
+        """Returns xMsg SUB socket
+
+        Returns:
+            sub_sock (zmq.Socket): connection SUB socket
+        """
+        return self.sub_sock
