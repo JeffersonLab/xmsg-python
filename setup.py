9509b20687723b7948f0338ac842f9df88fc1dd5
Ricardo Oyarzun <royarzun@gmail.com>
2015-08-03 13:00:45 -0300

    Add bin scripts (px_node and px_proxy) to user path after install

--- setup.py
+++ setup.py
@@ -81,5 +81,6 @@ if __name__ == '__main__':
           install_requires=['setuptools', 'pyzmq>=14.5.0', 'protobuf>=2.6',
                             'enum34>=1.0.4', 'argparse>=1.2.1',
                             'netifaces>=0.10.4', 'pytest', 'mockito',
-                            'sphinxcontrib-napoleon==0.3.11']
+                            'sphinxcontrib-napoleon==0.3.11'],
+          scripts=['bin/unix/px_node', 'bin/unix/px_proxy']
           )
