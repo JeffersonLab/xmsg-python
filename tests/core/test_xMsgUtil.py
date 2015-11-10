90449a30c7de7014c4c9510b7d53ee00e45a0f86
Ricardo Oyarzun <royarzun@gmail.com>
2015-11-10 13:48:37 -0300

    xMsgUtil: Refactor tests and fix disclaimer

--- tests/core/test_xMsgUtil.py
+++ tests/core/test_xMsgUtil.py
@@ -1,57 +1,54 @@
-'''
- Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
- Permission to use, copy, modify, and distribute this software and its
- documentation for educational, research, and not-for-profit purposes,
- without fee and without a signed licensing agreement.
-
- Author Vardan Gyurjyan
- Department of Experimental Nuclear Physics, Jefferson Lab.
-
- IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
- INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
- THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
- OF THE POSSIBILITY OF SUCH DAMAGE.
-
- JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
- HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
- SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-'''
+#
+# Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for educational, research, and not-for-profit purposes,
+# without fee and without a signed licensing agreement.
+#
+# Author Vardan Gyurjyan
+# Department of Experimental Nuclear Physics, Jefferson Lab.
+#
+# IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+# HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+#
 import unittest
 
-from xmsg.core.xMsgUtil import xMsgUtil
+from xmsg.core.xMsgUtil import xMsgUtil as utils
 from xmsg.data import xMsgRegistration_pb2
 
-IPREGX = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"+\
+IPREGX = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" + \
          "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 
 
 class TestXMsgUtil(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
     def test_build_registration(self):
-        register = xMsgUtil.build_registration("somename",
-                                               "somedesc",
-                                               "somedomain",
-                                               "somesubject",
-                                               "sometype",
-                                               True)
+        register = utils.build_registration("some name",
+                                            "some description",
+                                            "some domain",
+                                            "some subject",
+                                            "some type",
+                                            True)
         self.assertIsInstance(register,
-                              xMsgRegistration_pb2.xMsgRegistration) 
-            
+                              xMsgRegistration_pb2.xMsgRegistration)
+
     def test_get_local_ip(self):
-        test_case = xMsgUtil.get_local_ip()
+        test_case = utils.get_local_ip()
         self.assertIsInstance(test_case, basestring)
 
     def test_get_local_ips(self):
-        test_case = xMsgUtil.get_local_ips()
+        test_case = utils.get_local_ips()
         self.assertIsNot(len(test_case), 0)
         for ip in test_case:
             self.assertRegexpMatches(str(ip), IPREGX)
-            self.assertEqual(True, xMsgUtil.is_ip(ip))
+            self.assertEqual(True, utils.is_ip(ip))
 
 if __name__ == "__main__":
     unittest.main()
