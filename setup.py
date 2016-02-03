d07f1ef453125a96b6ac9e84b55e9397bb7e77f0
Ricardo Oyarzun <royarzun@gmail.com>
2016-02-03 15:13:12 -0300

    Reformat xmsg setup.py script: name and long description

--- setup.py
+++ setup.py
@@ -64,16 +64,17 @@ class xMsgInstall(install):
 
 
 if __name__ == '__main__':
-    setup(name='xMsg',
+    setup(name='xmsg',
           version='2.3',
-          description='xMsg is a lightweight, yet full featured publish/subscribe\
-            messaging system, presenting asynchronous publish/subscribe\
-            inter-process communication protocol: an API layer in Java, Python\
-            and C++.',
+          description='xMsg a lightweight, publish/subscribe messaging system',
+          long_description='xMsg is a lightweight, yet full featured ' +
+                           'publish/subscribe messaging system, presenting ' +
+                           'asynchronous publish/subscribe inter-process ' +
+                           'communication protocol: an API layer in Java, ' +
+                           'Python and C++.',
           author='Vardan Gyurgyan, Ricardo Oyarzun',
           author_email='vardan@jlab.org',
           url='https://claraweb.jlab.org',
-
           test_suite="tests",
           cmdclass={
             'test': xMsgTest,
@@ -81,9 +82,16 @@ if __name__ == '__main__':
           },
 
           packages=find_packages(exclude=["tests.*", "tests"]),
-          install_requires=['setuptools', 'pyzmq>=14.5.0', 'protobuf>=2.6',
-                            'enum34>=1.0.4', 'argparse>=1.2.1',
-                            'netifaces>=0.10.4', 'pytest', 'mockito'],
-          scripts=['bin/unix/px_node', 'bin/unix/px_proxy', 'bin/unix/px_publisher',
-                   'bin/unix/px_subscriber', 'bin/unix/px_sync_publisher']
+          install_requires=['setuptools',
+                            'pyzmq>=14.5.0',
+                            'protobuf>=2.6',
+                            'enum34>=1.0.4',
+                            'argparse>=1.2.1',
+                            'netifaces>=0.10.4',
+                            'pytest', 'mockito'],
+          scripts=['bin/unix/px_node',
+                   'bin/unix/px_proxy',
+                   'bin/unix/px_publisher',
+                   'bin/unix/px_subscriber',
+                   'bin/unix/px_sync_publisher']
           )
