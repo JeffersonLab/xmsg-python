998e4a67426557e82a41c844eddec4fd8971c3e5
Ricardo Oyarzun <royarzun@gmail.com>
2016-04-11 11:47:58 -0300

    xMsgTopic: add __repr__ magic method

--- xmsg/core/xMsgTopic.py
+++ xmsg/core/xMsgTopic.py
@@ -1,23 +1,4 @@
-#
-# Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
-# Permission to use, copy, modify, and distribute this software and its
-# documentation for educational, research, and not-for-profit purposes,
-# without fee and without a signed licensing agreement.
-#
-# Author Vardan Gyurjyan
-# Department of Experimental Nuclear Physics, Jefferson Lab.
-#
-# IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
-# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
-# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
-# OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
-# HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
-# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-#
+# coding=utf-8
 
 import re
 
@@ -86,6 +67,12 @@ class xMsgTopic(object):
         The topic string must be a valid xmsg topic
         """
         self.topic = topic
+
+    def __repr__(self):
+        return "<xMsgTopic: %s>" % self.topic
+
+    def __str__(self):
+        return self.topic
 
     @classmethod
     def build(cls, domain, subject=ANY, xtype=ANY):
@@ -201,6 +188,3 @@ class xMsgTopic(object):
             return match.group(group)
         else:
             raise MalformedCanonicalName("xMsgTopic: Received %s" % self.topic)
-
-    def __str__(self):
-        return self.topic
