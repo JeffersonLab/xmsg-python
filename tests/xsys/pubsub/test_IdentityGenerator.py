1302d56bfad4fb2aa26a8734063e85655baf1c73
Ricardo Oyarzun <royarzun@gmail.com>
2016-05-27 16:05:22 -0400

    Add IdentityGenerator class
    Identity generator will be used to identify connections
    in xMsg, this class is copied from its source in
    xMsg java.

---
+++ tests/xsys/pubsub/test_IdentityGenerator.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+import unittest
+
+from xmsg.xsys.pubsub.IdentityGenerator import IdentityGenerator
+
+
+class TestIdentityGenerator(unittest.TestCase):
+
+    def test_ctrlId_has_nine_digits(self):
+        self.assertEqual(9, len(str(IdentityGenerator.get_ctrl_id())))
+        self.assertEqual(9, len(str(IdentityGenerator.get_ctrl_id())))
+        self.assertEqual(9, len(str(IdentityGenerator.get_ctrl_id())))
+
+    def test_ctr_id_prefix_has_three_digits(self):
+        prefix1 = str(IdentityGenerator.get_ctrl_id())[1:4]
+        prefix2 = str(IdentityGenerator.get_ctrl_id())[1:4]
+        prefix3 = str(IdentityGenerator.get_ctrl_id())[1:4]
+
+        self.assertEqual(prefix1, prefix2)
+        self.assertEqual(prefix1, prefix3)
+        self.assertEqual(prefix3, prefix2)
+
+    def test_ctrl_id_first_digit_is_python_identifier(self):
+        self.assertEqual('3', str(IdentityGenerator.get_ctrl_id())[0])
+        self.assertEqual('3', str(IdentityGenerator.get_ctrl_id())[0])
+        self.assertEqual('3', str(IdentityGenerator.get_ctrl_id())[0])
+
+    def test_last_five_digits_are_random(self):
+        self.assertNotEqual(IdentityGenerator.get_ctrl_id(),
+                            IdentityGenerator.get_ctrl_id())
+        self.assertNotEqual(IdentityGenerator.get_ctrl_id(),
+                            IdentityGenerator.get_ctrl_id())
+        self.assertNotEqual(IdentityGenerator.get_ctrl_id(),
+                            IdentityGenerator.get_ctrl_id())
+
+
+if __name__ == "__main__":
+    unittest.main()
